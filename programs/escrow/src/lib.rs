9465ca0e39f88518adc83e019f6064bf80cdbc1d
ChiefWoods <chii.yuen@hotmail.com>
2025-01-30 21:49:51 +0800

    Init Anchor project

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("9w6Bay8y3yDdQzPLH8h4ygBQqDBDHjQEr3U2KqQHec9B");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
